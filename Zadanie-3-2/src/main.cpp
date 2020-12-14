9171f0636ccacd5d3a94176898c380ab269143f6
Przemyslaw Dzierlatka <Przemyslaw.Dzierlatka@student.wat.edu.pl>
2020-12-14 11:32:15 +0100

    Zakonczenie zajec  Przemyslaw Dzierlatka

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,16 +1,1 @@
-#include <Arduino.h>
-#include <avr/interrupt.h>
 
-int main(void){
-  DDRD &= ~(1 << DDD5);
-  PORTD |= (1 << PORTD5);
-  TIMSK1 |= (1 << TOIE1);
-  TCCR18 |= (1 <<CS12) | (1 << CS11) | (1 << CS10);
-  sei();
-  while(1) {
-
-  }
-}
-ISR(TIMER1_OVF_vect){
-  
-}
